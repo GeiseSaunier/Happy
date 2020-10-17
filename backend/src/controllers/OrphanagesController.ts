eb367eb2df390434fc63721872aa7ede2b709063
GeiseSaunier <geisesaunier3939@gmail.com>
2020-10-17 20:36:15 -0300

    Criando pasta back-end

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,65 @@
+import {Request, Response} from 'express';
+
+import { getRepository } from 'typeorm';
+
+import orphanage from '../models/orphanage';
+
+export default {
+
+    async index(request: Request, response: Response){
+
+        const orphanagesRepository = getRespository(Orphanage);
+
+        const orphanages = await orphanagesRepository.find();
+
+        return response.json(orphanages);
+       
+    },
+
+    async show(request: Request, response: Response){
+
+        const {id} = request.params;
+
+        const orphanagesRepository = getRespository(Orphanage);
+
+        const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+        return response.json(orphanage);
+    },
+
+    async create(request: Request, response: Response) {
+
+        const {
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_house,
+            open_on_weekends,
+        } = request.body;
+    
+    
+        const orphanagesRepository = getRepository(Orphanage);
+    
+        const orphanage = orphanagesRepository.create({
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_house,
+            open_on_weekends,
+    
+        });
+    
+        await orphanagesRepository.save(orphanage);
+    
+    
+        return response.status(201).json(orphanage);
+
+
+
+    }
+
+};
